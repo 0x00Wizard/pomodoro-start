4299dc4a17d9c8f39b5879247f739f517c1f1e49
wizardxxdx <wizardxd@icloud.com>
2023-05-02 21:25:15 -0700

    complete timer

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 2
-LONG_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 CHECK_MARK = "✔"
 reps = 0
 
@@ -18,27 +18,25 @@ reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    # global reps
+    global reps
+    reps += 1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    for i in range(8):
-        # If it's the 1st/3rd/5th/7th rep:
-        if i == 0 or i % 2 != 0:
-            count_down(work_sec)
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Break", fg=GREEN)
 
-        # If it's 2nd/4th/6th rep:
-        if i == 2 or i % 2 == 0:
-            count_down(short_break_sec)
-
-        # If it's the 8th rep:
-        if i == 8:
-            count_down(long_break_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
@@ -48,6 +46,9 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -64,7 +65,6 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-
 # Buttons
 start_button = Button(text="Start", highlightbackground=YELLOW, command=start_timer)
 start_button.grid(row=2, column=0)
@@ -75,7 +75,7 @@ reset_button.grid(row=2, column=2)
 # Labels
 timer_label = Label(text="Timer", fg=GREEN, background=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(row=0, column=1)
-check_label = Label(text="✔", fg=GREEN, background=YELLOW, bg=YELLOW)
+check_label = Label(fg=GREEN, background=YELLOW, bg=YELLOW)
 check_label.grid(row=3, column=1)
 
 window.mainloop()
