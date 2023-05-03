c48264d65e5a7d7bd2160361410cb40cb06631c4
wizardxxdx <wizardxd@icloud.com>
2023-05-02 19:10:03 -0700

    Complete UI

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+CHECK_MARK = "✔"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -18,9 +19,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Screen
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=, height=)
+# Canvas & Images
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Buttons
+start_button = Button(text="Start", highlightbackground=YELLOW)
+start_button.grid(row=2, column=0)
+reset_button = Button(text="Reset", highlightbackground=YELLOW)
+reset_button.grid(row=2, column=2)
+
+
+# Labels
+timer_label = Label(text="Timer", fg=GREEN, background=YELLOW, font=(FONT_NAME, 50))
+timer_label.grid(row=0, column=1)
+check_label = Label(text="✔", fg=GREEN, background=YELLOW, bg=YELLOW)
+check_label.grid(row=3, column=1)
+
 
 window.mainloop()
